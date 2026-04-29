7e06313d6498aaf25ca7bcd5cef95166c545c643
Haneen <haneen8924@gmail.com>
2026-04-29 20:00:14 +0300

    added by haneen

---
+++ pages.py/__init__.py
@@ -0,0 +1,5 @@
+from pages.restaurant_page import RestaurantPage
+from pages.menu_page import MenuPage
+from pages.item_details_page import ItemDetailsPage
+from pages.cart_page import CartPage
+from pages.checkout_page import CheckoutPage
